d133ee9e510c7298da92e08e4f0e56369c8fa020
klasing <klasing1959@gmail.com>
2017-11-16 12:53:21 +0100

    16-11-2017 12:53

--- WaitList/app/src/main/java/com/example/waitlist/MainActivity.java
+++ WaitList/app/src/main/java/com/example/waitlist/MainActivity.java
@@ -1,39 +1,13 @@
 package com.example.waitlist;
-/*
- * File -> Settings
- * Editor -> File and Code templates
- * select Class from list
- * enter following text in right window
- * #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end #parse("File Header.java") public class ${NAME} { }
- */
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    private GuestListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        RecyclerView waitlistRecyclerView;
-
-        waitlistRecyclerView = (RecyclerView) this.findViewById(R.id.all_guests_list_view);
-
-        waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        mAdapter = new GuestListAdapter(this);
-
-        waitlistRecyclerView.setAdapter(mAdapter);
-    }
-
-    public void addToWaitlist(View view) {
-
     }
 }
